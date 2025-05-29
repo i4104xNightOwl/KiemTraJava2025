138eaf27eb2889f15b4447f666f04d663ce1ac03
I4104 Night Owl <I4104.vn@gmail.com>
2025-05-29 09:33:50 +0700

    Cau_2_D

--- src/main/java/me/i4104/Main.java
+++ src/main/java/me/i4104/Main.java
@@ -3,10 +3,22 @@ package me.i4104;
 import me.i4104.Object.Employee;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ArrayList<Employee> employees = new ArrayList<>();
+
+        System.out.print("Nhap so luong nhan vien: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("-== Thong tin cho nhan vien thu " + (i + 1));
+            Employee employee = new Employee();
+            employee.init(scanner);
+            employees.add(employee);
+        }
         
     }
 }
