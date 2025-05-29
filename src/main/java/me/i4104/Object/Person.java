0416858236257fd52e7e6d7a4afffb0337bd0427
I4104 Night Owl <I4104.vn@gmail.com>
2025-05-29 09:23:15 +0700

    Cau_1

---
+++ src/main/java/me/i4104/Object/Person.java
@@ -0,0 +1,27 @@
+package me.i4104.Object;
+
+import java.util.Scanner;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public void init(Scanner scanner) {
+        try {
+            System.out.print("Nhap ten: ");
+            this.name = scanner.nextLine();
+
+            System.out.print("Nhap tuoi: ");
+            this.age = scanner.nextInt();
+        } catch (Exception e) {
+            System.out.println("Du lieu nhap vao khong hop le!");
+        }
+    }
+
+    public void show() {
+        System.out.println("|-------------------------------------|");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("|-------------------------------------|");
+    }
+}
